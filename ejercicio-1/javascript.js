66a3cdbce725353f8acf9e4eeb0467c9629c86dd
IFEE09 <ivanfaridespadas@gmail.com>
2022-09-05 20:19:04 -0500

    primera prueba de js

--- ejercicio-1/javascript.js
+++ ejercicio-1/javascript.js
@@ -3,4 +3,7 @@ let inputNombre = document.getElementById('nombre');
 inputNombre.addEventListener("click", inputVacio);
 
 function inputVacio() { 
+    if(inputNombre === "" || inputNombre === null) {
+        inputNombre.classList.add("input-vacio");
+    }
 }
